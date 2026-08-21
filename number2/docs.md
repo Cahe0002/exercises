e82a409314d9462184b415da00ea588a1e93b128
Caroline S <cahe0002@stud.ek.dk>
2026-08-21 08:26:50 +0200

    first try

--- number2/docs.md
+++ number2/docs.md
@@ -2,4 +2,21 @@
 
 Generate random number between 1 - 100 (computer)
 
+Declare variable for 1 - 100
+Declare variable for guesses count set to 0?
+Declare variable for to high number
+Declare variable for to low number
+Declare variable for right number
+
+Get the computers guess 
+Compare computer guess/input to the valid input values number
+If computer guess is invalid tell computer wrong number, to high or low
+else tell computer you got it right!
+
+Reset to start
+
+Explain how many try the computer has used
+
+
+
 
--- number2/number2.js
+++ number2/number2.js
@@ -1,10 +1,70 @@
-const Input = document.querySelector("#choose");
+const startButton = document.querySelector(".start");
 
-Input.addEventListener("click", chooseNumber);
+const computerGuess = document.querySelector(".guess");
+const tooHighButton = document.querySelector(".toohigh");
+const tooLowButton = document.querySelector(".toolow");
+const correctButton = document.querySelector(".correct");
 
-chooseNumber();
+let min = 1;
+let max = 100;
+let matRandom;
+let gameStarted = false;
 
-function chooseNumber() {
-  console.log(Math.ceil(Math.random() * 100));
-  Input.innerHTML = Math.ceil(Math.random() * 100);
+tooHighButton.disabled = true;
+tooLowButton.disabled = true;
+correctButton.disabled = true;
+
+tooHighButton.addEventListener("click", clicked);
+tooLowButton.addEventListener("click", clicked);
+correctButton.addEventListener("click", clicked);
+startButton.addEventListener("click", startGame);
+
+function startGame() {
+  gameStarted = true;
+  tooHighButton.disabled = false;
+  tooLowButton.disabled = false;
+  correctButton.disabled = false;
+  min = 1;
+  max = 100;
+
+  randomNumber();
 }
+
+function randomNumber() {
+  matRandom = Math.floor((min + max) / 2);
+  computerGuess.textContent = matRandom;
+}
+
+function clicked(e) {
+  if (gameStarted === false) {
+    return;
+  }
+  if (e.target === tooHighButton) {
+    max = matRandom - 1;
+    randomNumber();
+  } else if (e.target === tooLowButton) {
+    min = matRandom + 1;
+    randomNumber();
+  } else if (e.target === correctButton) {
+    if (matRandom === secretNumber) {
+      correctButton.textContent = "Computeren gættede rigtigt!";
+      startGame();
+    } else if (matRandom < secretNumber) {
+      console.log("det er for lavt");
+    }
+  }
+}
+
+// function computerWin(guess) {
+//   if (guess === secretNumber) {
+//     rightAnswer.textContent = "Du fik det rigtigt. Tillykke!";
+//   } else if (guess < secretNumber) {
+//     rightAnswer.textContent = "Tallet er for lavt";
+//     min = guess + 1;
+//     setTimeout(randomNumber, 5000);
+//   } else {
+//     rightAnswer.textContent = "Tallet er for højt";
+//     max = guess - 1;
+//     setTimeout(randomNumber, 5000);
+//   }
+// }
