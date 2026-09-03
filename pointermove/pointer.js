85fe63bab02ac1ec7af33dfd381bd0dd5f4a7b7a
Caroline S <cahe0002@stud.ek.dk>
2026-09-03 20:10:20 +0200

    mousemove_done

--- pointermove/pointer.js
+++ pointermove/pointer.js
@@ -1,5 +1,10 @@
-const myColor = document.getElementById("myColor");
+const myColor = document.getElementById("myColor"); // getting the id element from html
 
-myColor.addEventListener("mousemove", (e) => {
-  
-});
+document.addEventListener("mousemove", colorChange); // every time mouse move, call the function colorChange
+
+function colorChange(event) {
+  const x = (event.clientX / window.innerWidth) * 100; // left to right 
+  const y = (event.clientY / window.innerHeight) * 100; // top to bottom
+  myColor.style.setProperty("--light", `${x}%`);
+  myColor.style.setProperty("--sat", `${y}%`);
+}
